112f6c6298f607836ad2c33fd81eba33200aa5ce
ADITI <aditi23github@gmail.com>
2024-12-17 13:45:50 +0000

    polymorphism

---
+++ Polymorphism/Book.java
@@ -0,0 +1,12 @@
+class Book extends Publication{
+    private String author;
+
+    public int getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
---
+++ Polymorphism/Publication.java
@@ -0,0 +1,34 @@
+class Publication{
+    private String title;
+    private float price;
+    private int copies;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+    public double totalSales(){
+        return getPrice()*getCopies();
+    }
+}
